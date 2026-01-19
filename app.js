9d8c9cfae3d6337d7cf268fb83ccb5eee05bde4f
thongster <106802301+thongster@users.noreply.github.com>
2026-01-19 14:32:32 +0700

    Revised app.js, need to separate routes

--- app.js
+++ app.js
@@ -1,68 +1,28 @@
-/////// app.js
-
+// set up express
+require('dotenv').config();
 const path = require('node:path');
-const { Pool } = require('pg');
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
-
 const app = express();
+
+// set up ejs using views directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: false }));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+// middleware
+app.use(express.static(path.join(__dirname, 'public'))); // load static files
+app.use(express.urlencoded({ extended: true })); // parse form info
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query(
-        'SELECT * FROM users WHERE username = $1',
-        [username]
-      );
-      const user = rows[0];
-
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username' });
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        // passwords do not match!
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
+// set up sessions and passport
+const passport = require('./config/passport');
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
   })
 );
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [
-      id,
-    ]);
-    const user = rows[0];
-
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
@@ -97,9 +57,23 @@ app.get('/log-out', (req, res, next) => {
   });
 });
 
-app.listen(3000, (error) => {
+// error handling
+app.use((req, res) => {
+  res.status(404).send('<h1>Error 404</h1>');
+  //   res.status(404).render("404");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('<h1>Error 500</h1>');
+  //   res.status(500).render("500");
+});
+
+// start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
   if (error) {
     throw error;
   }
-  console.log('app listening on port 3000!');
+  console.log(`Server listening on port ${PORT}!`);
 });
