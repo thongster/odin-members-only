10a4cea63304a16c5a20baec502d8b8bde2c0ac2
thongster <106802301+thongster@users.noreply.github.com>
2026-01-24 11:21:33 +0700

    set all role routes and create show membership function

--- controllers/roleController.js
+++ controllers/roleController.js
@@ -1,2 +1,12 @@
 // import db queries
 const db = require('../db/queries');
+
+const showMembership = async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/?error=notloggedin');
+  }
+
+  res.render('membership', { title: 'Membership' });
+};
+
+module.exports = { showMembership };
--- routes/roleRouter.js
+++ routes/roleRouter.js
@@ -3,7 +3,9 @@ const roleRouter = Router();
 const roleController = require('../controllers/roleController');
 
 roleController.get('/membership', roleController.showMembership);
+roleController.post('/membership', roleController.changeMembership);
 
 roleController.get('/admin', roleController.showAdmin);
+roleController.post('/admin', roleController.changeAdmin);
 
 module.exports = roleRouter;
